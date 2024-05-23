b384464d2998c17c0c322c10b4bd7a17c687845c
«German <german.chubov@gmail.com>
2024-05-23 11:20:45 +0300

    feat: complete hard version of lesson 02

---
+++ Lesson 02/script-hard.js
@@ -0,0 +1,11 @@
+let num = 266219
+let mult = 1
+
+let numStr = num.toString()
+for (i = 0; i < numStr.length; ++i) {
+    mult *= +(numStr[i])
+}
+
+mult **= 3
+let multStr = mult.toString()
+console.log(multStr[0], multStr[1])
